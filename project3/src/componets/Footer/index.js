6bc67e98ee8d5779c6aa21748f83d194792a3c87
Jenny <jennyelisa07@aol.com>
2019-09-10 19:19:05 -0400

    routes working

---
+++ project3/src/componets/Footer/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return(
+        <footer id="pageFooter" className="footer text-center text-muted">
+        ​<div class="member-names">
+          <a href="https://github.com"  class="btn btn-secondary text-left mb-3 mr-4"><span
+              class="fab fa-github pr-2"></span>Portfolio:</a>
+          ​
+          <a href="https://github.com"  class="btn btn-secondary text-left mb-3 mr-4"><span
+              class="fab fa-github pr-2"></span>Portfolio:</a>
+          ​
+          <a href="https://github.com"  class="btn btn-secondary text-left mb-3 mr-4"><span
+              class="fab fa-github pr-2"></span>Portfolio:</a>
+          ​
+          ​
+          <a href="https://github.com"  class="btn btn-secondary text-left mb-3 mr-4"><span
+              class="fab fa-github pr-2"></span>Portfolio:</a>
+          ​</div>
+  
+      </footer>
+    );
+    
+}
+
+export default Footer;
