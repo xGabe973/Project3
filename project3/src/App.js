42847242f1b36e97142a4cc81a220748eaca4a01
xGabe973 <x973.alvarado@gmail.com>
2019-09-10 19:21:55 -0400

    folder structure and stuff

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Form from  './Components/Form/form.js'
+import Navbar from './Components/Navbar/navbar.js';
+import Wrapper from './Components/Wrapper/wrapper.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/SignOrLogIn" component={signUpLogIn} />
+          <Route exact path="/Profile" component={Profile} />
+          <Route exact path="/Workout" component={Workout} />
+          <Route exact path="/Nutrition" component={Nutrition} />  
+        </Wrapper>
+        <Footer />
+        </div>
+      </Router>
+     );
 }
 
 export default App;
